3b791badcbc0dca44018b5919835ad5960091501
Heenashaikh1103 <heenashaikh1.nmims@gmail.com>
2023-02-24 17:46:18 +0530

    attributes

--- app.js
+++ app.js
@@ -8,6 +8,11 @@ loginForm.addEventListener('submit', (event) => {
   if (username === 'heena' && password === '123') {
     sessionStorage.setItem('loggedIn', true);
     window.location.replace('home.html');
+    webengage.user.login('U-01'); //this is the unique user identifier being used here
+webengage.user.setAttribute('we_email', 'heenashaikh1.nmims@gmail.com'); //system user attribute
+webengage.user.setAttribute('we_birth_date', '1993-03-11');//system user attribute
+webengage.user.setAttribute('we_phone', '+919619377730');//system user attribute
+webengage.user.setAttribute("Age", 29); //custom user attribute
   } else {
     alert('Invalid username or password');
   }
